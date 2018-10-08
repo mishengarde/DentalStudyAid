d7c518214f4f8b3e8481ca16542ce0d16f45a187
Michelle <mun.michelless@gmail.com>
2018-10-08 20:03:31 +1100

    Add scripts
    Screenshot & turn off tute panel

---
+++ Assets/screenshot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TakeScreenshot : MonoBehaviour
+{
+    private int screenshotCount = 0;
+
+    // Check for screenshot key each frame
+    void Update()
+    {
+        // take screenshot on touch
+
+        if (Input.touches.Length > 0)
+        {
+            string screenshotFilename;
+            do
+            {
+                screenshotCount++;
+                screenshotFilename = "screenshot" + screenshotCount + ".png";
+
+            } while (System.IO.File.Exists(screenshotFilename));
+
+            //  audio.Play();
+
+            ScreenCapture.CaptureScreenshot(screenshotFilename);
+        }
+
+    }
+}
---
+++ Assets/turnofftute.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnofftute : MonoBehaviour
+{
+
+    public GameObject tutorialPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (PlayerPrefs.HasKey("HasRun"))
+        {
+            tutorialPanel.SetActive(false);
+
+        }
+
+        PlayerPrefs.SetInt("HasRun", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
