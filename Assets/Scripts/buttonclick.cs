0bbbeabef53868f637966ab24ec5c777fa31bf81
Michelle <mun.michelless@gmail.com>
2018-10-29 14:33:02 +1100

    Made lower colliders and error msgs

---
+++ Assets/Scripts/buttonclick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonclick : MonoBehaviour
+{
+
+    public AudioSource sound;
+    private Rect audiorect;
+
+    void Start()
+    {
+        audiorect = new Rect(20, 20, 100, 20);
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Label(audiorect, "Play Audio");
+
+        if (audiorect.Contains(Event.current.mousePosition))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                sound.Play();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/fadeout.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeout : MonoBehaviour
+{
+    public GameObject objectToFade;
+
+    public void fadeOut()
+    {
+        StartCoroutine(doFade());
+    }
+
+    IEnumerator doFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 1;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        objectToFade.SetActive(false);
+        yield return null;
+    }
+}
